c81f50f21c9b9d3524034f888a1a696f0a6ffa57
Thaarani <thaaranis13@gmail.com>
2022-11-13 19:27:59 -0500

    completed

---
+++ Linked List Cycle.py
@@ -0,0 +1,16 @@
+#TC: O(n)
+#SC: O(1)
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = head
+        fast = head
+        while fast!=None and fast.next!=None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
